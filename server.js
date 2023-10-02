a4406f645772b675d3d6eef76a5becb70e7cd8ac
Raj <SoftDevRaj77@gmail.com>
2023-10-01 20:19:30 -0400

    moved notes.html to root folder and adjusted server.js to reflect the change

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 
 // Serve the notes page
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./notes.html"));
 });
 
 // Fetch all notes from the db.json file
