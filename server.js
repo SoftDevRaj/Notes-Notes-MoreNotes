76907652811d26193099b8b54d3476143da49a2d
Raj <SoftDevRaj77@gmail.com>
2023-10-02 00:49:49 -0400

    put index and notes back into public folder

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.static("public"));
 
 // Serve the main page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Serve the notes page
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // Fetch all notes from the db.json file
