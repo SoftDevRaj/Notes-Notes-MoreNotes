2584179fe68842d03178090f3d2092f070c4f5c1
Raj <SoftDevRaj77@gmail.com>
2023-10-01 20:17:21 -0400

    adjusted code to reflect moved index.html file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 // Serve the main page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 // Serve the notes page
